74867b1529f7ef0d4071291dc5fdc00c7180b170
Brito <>
2021-05-08 13:38:25 +0000

    rotas hello e api

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
@@ -10,3 +10,12 @@ def home():
     <p>Desenvolvimento web com Python e Flask</p>
     <p>Bruno Rocha</p>
     ''')
+
+@app.route('/hello')
+def hello():
+    return "Hello live Python"
+
+
+@app.route('/api')
+def api_demo():
+    return jsonify(data={'Key': 'Value'})
