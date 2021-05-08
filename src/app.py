acef742539ccdc31e78fe1454881e56f3c1ca387
Brito <>
2021-05-08 14:28:09 +0000

    Aplicado estrutura para blueprint+factory

--- src/app.py
+++ src/app.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from src import views
+from src import contact
 
 
 def create_app():
     app = Flask(__name__)
     views.configure(app)
     # configurar extensões
+    contact.configure(app)
     # configurar variáveis
     return app
