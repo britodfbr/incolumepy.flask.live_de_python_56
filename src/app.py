d2038fb9cd7f07f8f4dd2412e2fdb7228eff08b5
Brito <>
2021-05-10 02:08:19 +0000

    adicionado pacote flask-admin e configuração para sqlite3

--- src/app.py
+++ src/app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from . import views
 from . import contact
+from . import db
 
 def create_app():
     app = Flask(__name__)
-    views.configure(app)
     # configurar extensões
+    db.configure(app)
+    views.configure(app)
     contact.configure(app)
     # configurar variáveis
     return app
